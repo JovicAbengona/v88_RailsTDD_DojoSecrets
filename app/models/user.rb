ebc8b0c853d0289c361f34ade4e06cb537490d38
Jovic Abengona <jvlabengona@gmail.com>
2021-07-02 14:13:36 +0800

    Initial Commit

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ApplicationRecord
+  has_secure_password
+
+  EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]+)\z/i
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: EMAIL_REGEX }
+  validates :password_digest, presence: true, length: { minimum: 8 }
+
+  # this callback will run before saving on create and update
+  before_save :downcase_email
+
+  private
+      def downcase_email
+          self.email.downcase!
+      end
+end
