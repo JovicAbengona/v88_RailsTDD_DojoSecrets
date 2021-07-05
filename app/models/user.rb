e7e851251f614fd8c86eb6e4adbf0a453ea2dd35
Jovic Abengona <jvlabengona@gmail.com>
2021-07-05 11:17:52 +0800

    Completed tests for User and Secret model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   has_secure_password
 
+  has_many :secrets
+  has_many :likes, dependent: :destroy
+  has_many :secrets_liked, through: :likes, source: :secret
+
   EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]+)\z/i
 
   validates :name, presence: true
